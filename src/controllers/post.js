ef3a0cd3bd9c9d98962e3b82ca6235a8abee4674
Paul Smart <pdsmart6@gmail.com>
2022-04-26 18:13:18 +0100

    getAll controller implemented

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,28 +1,33 @@
+import dbClient from '../utils/dbClient.js'
 import { sendDataResponse } from '../utils/responses.js'
 
 export const create = async (req, res) => {
   const { content } = req.body
-
+  console.log(req.user)
   if (!content) {
     return sendDataResponse(res, 400, { content: 'Must provide content' })
   }
-
-  return sendDataResponse(res, 201, { post: { id: 1, content } })
+  const createdPost = await dbClient.post.create({
+    data: {
+      content: content,
+      user: {
+        connect: {
+          id: req.user.id
+        }
+      }
+    }
+  })
+  console.log('my post', createdPost)
+  return sendDataResponse(res, 201, { post: createdPost })
 }
 
 export const getAll = async (req, res) => {
+  const allPosts = await dbClient.post.findMany({
+    include: {
+      user: true
+    }
+  })
   return sendDataResponse(res, 200, {
-    posts: [
-      {
-        id: 1,
-        content: 'Hello world!',
-        author: { ...req.user }
-      },
-      {
-        id: 2,
-        content: 'Hello from the void!',
-        author: { ...req.user }
-      }
-    ]
+    posts: allPosts
   })
 }
--- src/routes/post.js
+++ src/routes/post.js
@@ -1,12 +1,10 @@
 import { Router } from 'express'
 import { create, getAll } from '../controllers/post.js'
-// import { validateAuthentication } from '../middleware/auth.js'
+import { validateAuthentication } from '../middleware/auth.js'
 
 const router = Router()
 
-// router.post('/', validateAuthentication, create)
-// router.get('/', validateAuthentication, getAll)
-router.post('/', create)
-router.get('/', getAll)
+router.post('/', validateAuthentication, create)
+router.get('/', validateAuthentication, getAll)
 
 export default router
