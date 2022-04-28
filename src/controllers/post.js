70160b8fcb24277bce9ead712ecfa024b10e2f69
Elvis Onobhayedo <elvisonob@yahoo.com>
2022-04-28 10:49:03 +0100

    updated create comment

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -4,7 +4,7 @@ import { sendDataResponse } from '../utils/responses.js'
 
 export const create = async (req, res) => {
   const { content } = req.body
-
+  
   if (!content) {
     return sendDataResponse(res, 400, { content: 'Must provide content' })
   }
@@ -21,6 +21,32 @@ export const create = async (req, res) => {
   console.log('my post', createdPost)
   return sendDataResponse(res, 201, { post: createdPost })
 }
+
+export const createComment = async (req, res) => {
+
+  const {content, postId} = req.body
+
+  if (!content) {
+    return sendDataResponse(res, 400, {content: 'Must provide content'})
+  }
+   const createdComment = await dbClient.postComment.create({   
+        data: {
+            content: content,
+            post:{
+              connect: {
+                id:postId
+              }
+            },
+            user: {
+              connect: {
+                id: req.user.id
+              }
+            }
+          }
+      })
+      return sendDataResponse(res, 201, { comment: createdComment })
+}
+
 
 export const getAll = async (req, res) => {
   return sendDataResponse(res, 200, {
