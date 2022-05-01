943e82eb4c1d8c496e35c720a70a6de40cdb56f6
Paul Smart <pdsmart6@gmail.com>
2022-05-01 23:01:41 +0100

    updated controller

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,25 +1,76 @@
 import dbClient from '../utils/dbClient.js'
-
+import Joi from 'joi'
 import { sendDataResponse } from '../utils/responses.js'
 
 export const create = async (req, res) => {
-  const { content } = req.body
-
-  if (!content) {
-    return sendDataResponse(res, 400, { content: 'Must provide content' })
+  const schema = Joi.object({
+    content: Joi.string().min(3).max(150).required()
+  })
+  const { error, value } = schema.validate(req.body)
+  if (error) {
+    return sendDataResponse(res, 400, { error: error.details[0].message })
   }
-  const createdPost = await dbClient.post.create({
-    data: {
-      content: content,
-      user: {
-        connect: {
-          id: req.user.id
+  const { content } = value
+  try {
+    const createdPost = await dbClient.post.create({
+      data: {
+        content: content,
+        user: {
+          connect: {
+            id: req.user.id
+          }
+        }
+      },
+      include: {
+        user: {
+          include: {
+            profile: true
+          }
         }
       }
-    }
+    })
+    return sendDataResponse(res, 201, { post: createdPost })
+  } catch (e) {
+    return sendDataResponse(res, 500, { error: e.message })
+  }
+}
+
+export const createComment = async (req, res) => {
+  const schema = Joi.object({
+    content: Joi.string().min(3).max(150).required()
   })
-  console.log('my post', createdPost)
-  return sendDataResponse(res, 201, { post: createdPost })
+  const { error, value } = schema.validate(req.body)
+  if (error) {
+    return sendDataResponse(res, 400, { error: error.details[0].message })
+  }
+  const { content } = value
+  const { postId } = req.params
+  const commentOnPost = await dbClient.post.findUnique({
+    where: { id: parseInt(postId) }
+  })
+  if (!commentOnPost) {
+    return sendDataResponse(res, 404, { error: 'Post not found' })
+  }
+  try {
+    const createdComment = await dbClient.postComment.create({
+      data: {
+        content: content,
+        post: {
+          connect: {
+            id: parseInt(postId)
+          }
+        },
+        user: {
+          connect: {
+            id: req.user.id
+          }
+        }
+      }
+    })
+    return sendDataResponse(res, 201, { comment: createdComment })
+  } catch (e) {
+    return sendDataResponse(res, 500, { error: e.message })
+  }
 }
 
 export const getAll = async (req, res) => {
