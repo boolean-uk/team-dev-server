3abcb418875ddbd36d7d94994a8cad59d03b9085
Paul Smart <pdsmart6@gmail.com>
2022-05-04 18:29:24 +0100

    added DELETE like route + refactoring

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,5 +1,7 @@
 import { Router } from 'express'
-import { create, getAll, createComment, likePost } from '../controllers/post.js'
+import { create, getAll } from '../controllers/posts/post.js'
+import { createComment } from '../controllers/posts/comments.js'
+import { likePost, unlikePost } from '../controllers/posts/likes.js'
 import { validateAuthentication } from '../middleware/auth.js'
 
 const router = Router()
@@ -8,5 +10,6 @@ router.post('/', validateAuthentication, create)
 router.get('/', validateAuthentication, getAll)
 router.post('/:postId/comment', validateAuthentication, createComment)
 router.post('/:postId/like', validateAuthentication, likePost)
+router.delete('/like/:likeId', validateAuthentication, unlikePost)
 
 export default router
