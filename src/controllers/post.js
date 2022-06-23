12b06ca51ed129fe3a6bf9b40dfabe40b9c7b6de
Tibor22 <tothtibitravel@gmail.com>
2022-06-23 09:49:56 +0100

    created post in server/controller/post.js

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,15 +1,24 @@
 import { sendDataResponse } from '../utils/responses.js'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 export const create = async (req, res) => {
   const { content } = req.body
-
   if (!content) {
     return sendDataResponse(res, 400, { content: 'Must provide content' })
   }
-
-  return sendDataResponse(res, 201, { post: { id: 1, content } })
+  try {
+    const post = await prisma.post.create({
+      data: {
+        content,
+        userId: req.user.id
+      }
+    })
+    return sendDataResponse(res, 201, { ...post })
+  } catch (err) {
+    return sendDataResponse(res, 400, { err: err.message })
+  }
 }
-
 export const getAll = async (req, res) => {
   return sendDataResponse(res, 200, {
     posts: [
