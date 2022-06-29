d2ff4ddf6af9ffdd44e8e2d3bf310658a30d27f6
Anica Rahman <anicarahman2009@yahoo.it>
2022-06-29 17:04:58 +0100

    removed console.log and linter ignores the non-camel case variables

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -18,7 +18,6 @@ export const login = async (req, res) => {
     const areCredentialsValid = await validateCredentials(password, foundUser)
 
     if (!areCredentialsValid) {
-      console.log('PASSWORD DONT MACTH')
       return sendDataResponse(res, 400, {
         email: 'Invalid email and/or password provided'
       })
