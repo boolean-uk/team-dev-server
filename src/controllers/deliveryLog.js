e55adc6d130428ed4216a4d1435f7e5bdac07293
Paulo Barata <pbara7a@gmail.com>
2022-07-06 12:28:10 +0100

    Refactored based on PR feedback.

--- src/controllers/deliveryLog.js
+++ src/controllers/deliveryLog.js
@@ -14,18 +14,11 @@ export const create = async (req, res) => {
   const deliveryLogToCreate = await DeliveryLog.fromJson(newDeliveryLogData)
   const deliveryLog = await deliveryLogToCreate.save()
 
-  const deliveryLogLines = []
-
-  for (const line of lines) {
-    const lineContent = line.content
-    const json = { content: lineContent, logId: deliveryLog.id }
-    const newDeliveryLogLineData = await DeliveryLogLines.fromJson(json)
-    deliveryLogLines.push(newDeliveryLogLineData)
-  }
-
-  for (const deliveryLogLineToCreate of deliveryLogLines) {
-    deliveryLogLineToCreate.save()
-  }
+  lines.map(async (line) => {
+    const data = { content: line.content, logId: deliveryLog.id }
+    const logLineToCreate = await DeliveryLogLines.fromJson(data)
+    return await logLineToCreate.save()
+  })
 
   return sendDataResponse(res, 201, { deliveryLog })
 }
