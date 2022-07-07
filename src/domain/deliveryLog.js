f03c1b9ff26b7b2601e170c687786b7c5b3e9566
Anica Rahman <anicarahman2009@yahoo.it>
2022-07-07 16:19:44 +0100

    can GET all delivery logs and GET delivery log by id

--- src/domain/deliveryLog.js
+++ src/domain/deliveryLog.js
@@ -1,5 +1,24 @@
 import dbClient from '../utils/dbClient.js'
 
+export async function getDeliveryLogs() {
+  const DeliveryLogs = await dbClient.DeliveryLog.findMany({
+    include: { user: { include: { profile: true } }, lines: true }
+  })
+
+  return DeliveryLogs
+}
+
+export async function getDeliveryLog(id) {
+  id = Number(id)
+  const deliveryLog = await dbClient.DeliveryLog.findUnique({
+    where: { id },
+    include: { user: { include: { profile: true } }, lines: true },
+    rejectOnNotFound: true
+  })
+
+  return deliveryLog
+}
+
 export default class DeliveryLog {
   static fromDb(deliveryLog) {
     return new DeliveryLog(
