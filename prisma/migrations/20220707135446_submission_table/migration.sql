229631a729bb05588215e01be5fc9c4ca6d98d79
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-07-08 10:04:28 +0100

    Submission migration updated

--- prisma/migrations/20220707135446_submission_table/migration.sql
+++ prisma/migrations/20220707135446_submission_table/migration.sql
@@ -4,9 +4,6 @@
   - You are about to drop the column `isEditted` on the `Note` table. All the data in the column will be lost.
 
 */
--- AlterTable
-ALTER TABLE "Note" DROP COLUMN "isEditted",
-ADD COLUMN     "isEdited" BOOLEAN NOT NULL DEFAULT false;
 
 -- CreateTable
 CREATE TABLE "CohortExercise" (
